bd7b23c00873df092da6b7e3e55edba0e88fa10a
goatchris\Cristian <saaepear@gmail.com>
2026-07-06 20:19:48 -0500

    feat: creé main application layout component con su integracion vite

---
+++ resources/components/app.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>{{ $title ?? 'Pokédex' }}</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body class="bg-gray-100 min-h-screen">
+    {{ $slot }}
+</body>
+</html>
